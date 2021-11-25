7f76440e8ee4ff46e5e090adfc1cb74d074a4911
Erick Duarte <erickod@gmail.com>
2021-11-25 19:55:19 -0300

    refactor: is using IEnvironment

--- clicksignlib/handlers/template_handler/template_handler.py
+++ clicksignlib/handlers/template_handler/template_handler.py
@@ -1,4 +1,7 @@
+from clicksignlib.environments.protocols import IEnvironment
+
+
 class TemplateHandler:
-    def __init__(self, *, access_token: str, environment: str) -> None:
+    def __init__(self, *, access_token: str, environment: IEnvironment) -> None:
         self._access_token = access_token
         self._environment = environment
