d0ca706c2c1ff11cf24ebdd01b2105d94983e170
Erick Duarte <erickod@gmail.com>
2021-11-25 13:03:32 -0300

    test: ensure EnviromentABC exists in environments package

--- tests/test_environments.py
+++ tests/test_environments.py
@@ -3,3 +3,7 @@ import clicksignlib
 
 def test_enviroments_package_exists() -> None:
     assert hasattr(clicksignlib, "environments")
+
+
+def test_EnviromentABC_exists_in_environments_package() -> None:
+    assert hasattr(clicksignlib.environments, "EnviromentABC")
