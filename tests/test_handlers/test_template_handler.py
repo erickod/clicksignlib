833a5be8238fafa1f0919a3331d6534e5bf9ddcf
Erick Duarte <erickod@gmail.com>
2021-11-25 16:24:10 -0300

    fix: fixed text

--- tests/test_handlers/test_template_handler.py
+++ tests/test_handlers/test_template_handler.py
@@ -5,7 +5,7 @@ def test_ensure_TemplateHandler_can_be_imported_from_clicksignlib_handlers_packa
     assert hasattr(clicksignlib.handlers, "TemplateHandler")
 
 
-def test_ensure_TemplateHandler_needs_a_access_token_when_its_intantiated() -> None:
+def test_ensure_TemplateHandler_needs_an_access_token_when_its_intantiated() -> None:
     access_token = "any valid token"
     sut = clicksignlib.handlers.TemplateHandler(access_token=access_token)
     assert sut._access_token == access_token
