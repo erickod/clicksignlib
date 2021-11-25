055c0a1253853c3265b9bca9fdfb62c2a56a8beb
Erick Duarte <erickod@gmail.com>
2021-11-25 19:59:09 -0300

    feat: Template can receiva access_token and environment as param

--- clicksignlib/handlers/template_handler/template.py
+++ clicksignlib/handlers/template_handler/template.py
@@ -1,2 +1,7 @@
+from clicksignlib.environments.protocols import IEnvironment
+
+
 class Template:
-    ...
+    def __init__(self, access_token:str, environment:IEnvironment) -> None:
+        self._access_token = access_token
+        self._environment = environment
