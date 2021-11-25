5429be614a1dc76495f80367be1268da3a02a563
Erick Duarte <erickod@gmail.com>
2021-11-25 16:19:35 -0300

    test: ensure token is requires and is saved as class attr

--- tests/test_handlers/test_template_handler.py
+++ tests/test_handlers/test_template_handler.py
@@ -1,5 +1,11 @@
 import clicksignlib
 
 
-def test_clicksignlib_has_a_handlers_package() -> None:
+def test_ensure_TemplateHandler_can_be_imported_from_clicksignlib_handlers_package() -> None:
     assert hasattr(clicksignlib.handlers, "TemplateHandler")
+
+
+def test_ensure_TemplateHandler_needs_a_access_token_when_its_intantiated() -> None:
+    access_token = "any valid token"
+    sut = clicksignlib.handlers.TemplateHandler(access_token=access_token)
+    assert sut._access_token == access_token
