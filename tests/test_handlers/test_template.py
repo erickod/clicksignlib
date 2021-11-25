501b7f6a5d372ebfd36796d7d6bfbb6c332d8893
Erick Duarte <erickod@gmail.com>
2021-11-25 19:59:30 -0300

    test: Ensure Template can receiva access_token and environment as param

--- tests/test_handlers/test_template.py
+++ tests/test_handlers/test_template.py
@@ -1,5 +1,17 @@
-import clicksignlib
+from clicksignlib.environments import SandboxEnvironment
+from clicksignlib.handlers import Template
 
 
 def test_handlers_has_a_template_package() -> None:
+    import clicksignlib
+
     assert hasattr(clicksignlib.handlers, "Template")
+
+
+def test_Template_instantiation_parmas() -> None:
+    access_token = "any valid access token"
+    env = SandboxEnvironment()
+    sut = Template(access_token=access_token, environment=env)
+
+    assert sut._access_token == access_token
+    assert sut._environment == env
