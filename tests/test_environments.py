e7a85c90586712d38370d79ae426b84705448b93
Erick Duarte <erickod@gmail.com>
2021-11-25 15:04:32 -0300

    test: add is_valid_method

--- tests/test_environments.py
+++ tests/test_environments.py
@@ -1,4 +1,6 @@
 import clicksignlib
+import pytest
+from clicksignlib.environments import EnvironmentABC, ProductionEnvironment
 
 
 def test_enviroments_package_exists() -> None:
@@ -15,3 +17,9 @@ def test_TestEnviroment_exists_in_environments_package() -> None:
 
 def test_ProductionEnvironment_exists_in_environments_package() -> None:
     assert hasattr(clicksignlib.environments, "ProductionEnvironment")
+
+
+def test_EnvironmentABC_raises_on_calling_is_valid() -> None:
+    with pytest.raises(NotImplementedError):
+        sut = EnvironmentABC()
+        sut.is_valid()
