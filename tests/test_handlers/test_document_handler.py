7d39e98e5d756f0b308dc7111f33ed7459200198
Erick Duarte <erickod@gmail.com>
2021-11-26 15:11:22 -0300

    test: DocumentHandler instantiation params

--- tests/test_handlers/test_document_handler.py
+++ tests/test_handlers/test_document_handler.py
@@ -1,5 +1,17 @@
 import clicksignlib
 
+access_token = "any valid token"
+api_version = "/api/v2"
+
 
 def test_TemplateHandler_can_be_imported_from_handlers_package() -> None:
     assert hasattr(clicksignlib.handlers, "DocumentHandler")
+
+
+def test_ensure_TemplateHandler_intantiation_params() -> None:
+    env = clicksignlib.environments.SandboxEnvironment()
+    sut = clicksignlib.handlers.DocumentHandler(
+        access_token=access_token, environment=env, api_version=api_version
+    )
+    assert sut._access_token == access_token
+    assert sut._environment == env
