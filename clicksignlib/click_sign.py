44393ebfcdde34e6a7594ec1ec5bd59a8ec37730
Erick Duarte <erickod@gmail.com>
2021-11-26 09:49:24 -0300

    feat: add template property

--- clicksignlib/click_sign.py
+++ clicksignlib/click_sign.py
@@ -1,6 +1,7 @@
 import requests
 
 from clicksignlib.environments.protocols import IEnvironment
+from clicksignlib.handlers import TemplateHandler
 
 
 class ClickSign:
@@ -14,3 +15,12 @@ class ClickSign:
         self._access_token = access_token
         self._environment = environment
         self._requests_adapter = requests_adapter
+
+    @property
+    def template(self) -> TemplateHandler:
+        return TemplateHandler(
+            access_token=self._access_token,
+            environment=self._environment,
+            requests_adapter=self._requests_adapter,
+            api_version="/api/v2",
+        )
