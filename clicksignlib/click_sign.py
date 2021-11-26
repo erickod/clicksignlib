7e390bda19e22ff8142e62c1742239e734810c84
Erick Duarte <erickod@gmail.com>
2021-11-26 09:40:20 -0300

    feat: ClickSign accept access_token, environment and request_adapt on constructor

--- clicksignlib/click_sign.py
+++ clicksignlib/click_sign.py
@@ -1,2 +1,16 @@
+import requests
+
+from clicksignlib.environments.protocols import IEnvironment
+
+
 class ClickSign:
-    ...
+    def __init__(
+        self,
+        *,
+        access_token: str,
+        environment: IEnvironment,
+        requests_adapter=requests,
+    ) -> None:
+        self._access_token = access_token
+        self._environment = environment
+        self._requests_adapter = requests_adapter
