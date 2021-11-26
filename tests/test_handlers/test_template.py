e9c951cec425af743b07ce9eab1abae401a8a26b
Erick Duarte <erickod@gmail.com>
2021-11-25 21:15:05 -0300

    test: base_endpoint property

--- tests/test_handlers/test_template.py
+++ tests/test_handlers/test_template.py
@@ -2,6 +2,9 @@ import requests
 from clicksignlib.environments import SandboxEnvironment
 from clicksignlib.handlers import Template
 
+name = "any valid template name"
+access_token = "any valid access token"
+
 
 def test_handlers_has_a_template_package() -> None:
     import clicksignlib
@@ -10,8 +13,6 @@ def test_handlers_has_a_template_package() -> None:
 
 
 def test_Template_instantiation_params() -> None:
-    name = "any valid template name"
-    access_token = "any valid access token"
     env = SandboxEnvironment()
     sut = Template(name=name, access_token=access_token, environment=env)
 
@@ -20,3 +21,9 @@ def test_Template_instantiation_params() -> None:
     assert sut._access_token == access_token
     assert sut._environment == env
     assert sut._requests == requests
+
+
+def test_Template_base_endpoint_returns_the_env_endpoint_() -> None:
+    env = SandboxEnvironment()
+    sut = Template(name=name, access_token=access_token, environment=env)
+    sut.base_endpoint == env.endpoint
