1277113da6ce3b674399e326365864a27a22ac86
Erick Duarte <erickod@gmail.com>
2021-11-26 12:29:56 -0300

    test: ensure from_bytes method save an encoded64 data in content

--- tests/test_handlers/test_document.py
+++ tests/test_handlers/test_document.py
@@ -1,4 +1,17 @@
+from clicksignlib.handlers import Document
+
+
 def test_handlers_has_a_Document_class() -> None:
     import clicksignlib
 
     assert hasattr(clicksignlib.handlers, "Document")
+
+
+def test_Document_from_bytes_method() -> None:
+    file_bytes = b"testing data"
+    filename = "test.test.docx"
+    sut = Document()
+    sut.from_bytes(filename, file_bytes)
+
+    assert sut._content
+    assert type(sut._content) is str
