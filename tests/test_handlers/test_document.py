8c016ca81f4aa860e42c6931c0ac2d552b26989c
Erick Duarte <erickod@gmail.com>
2021-11-26 12:43:02 -0300

    test: ensure from_file calls from_bytes method

--- tests/test_handlers/test_document.py
+++ tests/test_handlers/test_document.py
@@ -1,4 +1,9 @@
+from unittest.mock import Mock
+
 from clicksignlib.handlers import Document
+
+filename = "test.docx"
+file_bytes = b"testing data"
 
 
 def test_handlers_has_a_Document_class() -> None:
@@ -8,10 +13,17 @@ def test_handlers_has_a_Document_class() -> None:
 
 
 def test_Document_from_bytes_method() -> None:
-    file_bytes = b"testing data"
-    filename = "test.test.docx"
     sut = Document()
     sut.from_bytes(filename, file_bytes)
 
     assert sut._content
     assert type(sut._content) is str
+
+
+def test_Document_from_file_method_calls_from_bytes() -> None:
+    with open(filename, "rb") as f:
+        sut = Document()
+        sut.from_bytes = Mock()
+        sut.from_file(filename)
+
+        sut.from_bytes.assert_called_with(filename, f.read())
