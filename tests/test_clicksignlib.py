bb0cb36e77156e45d7a7783f853651ec52466879
Erick Duarte <erickod@gmail.com>
2021-11-26 09:41:00 -0300

    test: ensure access_token|environment|request_adapter on constructor of ClickSign

--- tests/test_clicksignlib.py
+++ tests/test_clicksignlib.py
@@ -1,9 +1,27 @@
-import clicksignlib
+from unittest.mock import Mock
+
+from clicksignlib import ClickSign, __version__
+from clicksignlib.environments import SandboxEnvironment
 
 
 def test_version() -> None:
-    assert clicksignlib.__version__ == "0.1.0"
+    assert __version__ == "0.1.0"
 
 
 def test_clicksignlib_has_a_ClickSign_class_to_import() -> None:
     from clicksignlib import ClickSign
+
+    assert ClickSign
+
+
+def test_ClickSign_instantiation_params() -> None:
+    access_token = "any valid token"
+    env = SandboxEnvironment()
+    requests = Mock()
+
+    sut = ClickSign(
+        access_token=access_token, environment=env, requests_adapter=requests
+    )
+    assert sut._access_token == access_token
+    assert sut._environment == env
+    assert sut._requests_adapter == requests
