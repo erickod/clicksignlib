d0af0c84eaf309756e5dccac437f3e2b863a0d6d
Erick Duarte <erickod@gmail.com>
2021-11-26 14:28:06 -0300

    feat: add DocumentHandler class

--- clicksignlib/handlers/__init__.py
+++ clicksignlib/handlers/__init__.py
@@ -1,4 +1,5 @@
 from . import mixins
+from .document_handler import DocumentHandler
 from .document_handler.document import Document
 from .template_handler import TemplateHandler
 from .template_handler.template import Template
