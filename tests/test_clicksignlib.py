b56d6b039f27ae4a74334f7a7522d105ecd21d00
Erick Duarte <erickod@gmail.com>
2021-11-26 09:30:31 -0300

    test: ensure ClickSign Class can be imported

--- tests/test_clicksignlib.py
+++ tests/test_clicksignlib.py
@@ -1,5 +1,9 @@
-from clicksignlib import __version__
+import clicksignlib
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_version() -> None:
+    assert clicksignlib.__version__ == "0.1.0"
+
+
+def test_clicksignlib_has_a_ClickSign_class_to_import() -> None:
+    from clicksignlib import ClickSign
