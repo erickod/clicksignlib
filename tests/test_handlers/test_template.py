b4a7412f60f0830aa7e03bed9f347bbd965363d3
Erick Duarte <erickod@gmail.com>
2021-11-25 21:29:24 -0300

    test: ensure bytes is saving on _content attr

--- tests/test_handlers/test_template.py
+++ tests/test_handlers/test_template.py
@@ -3,6 +3,7 @@ from clicksignlib.handlers import Template
 
 name = "any valid template name"
 access_token = "any valid access token"
+data_bytes = b"any byte value"
 
 
 def test_handlers_has_a_template_package() -> None:
@@ -20,3 +21,11 @@ def test_Template_instantiation_params() -> None:
     assert sut._access_token == access_token
     assert sut._environment == env
     assert sut._status_code == 0
+
+
+def test_Template_from_bytes_method_is_saving_is_saved_in_content_property() -> None:
+    env = SandboxEnvironment()
+    sut = Template(name=name, access_token=access_token, environment=env)
+    sut.from_bytes(data_bytes)
+
+    assert sut._content == data_bytes
