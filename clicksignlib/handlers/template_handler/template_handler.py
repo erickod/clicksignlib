e07c7245301e4da8a0ecb2c36e31e837ceacf33a
Erick Duarte <erickod@gmail.com>
2021-11-25 22:04:15 -0300

    feat: add api_version param on constructor

--- clicksignlib/handlers/template_handler/template_handler.py
+++ clicksignlib/handlers/template_handler/template_handler.py
@@ -8,11 +8,13 @@ class TemplateHandler:
         *,
         access_token: str,
         environment: IEnvironment,
+        api_version: str,
         requests_adapter=requests,
     ) -> None:
         self._access_token = access_token
         self._environment = environment
         self._requests = requests_adapter
+        self._api_version = api_version
 
     @property
     def base_endpoint(self) -> str:
--- tests/test_handlers/test_template_handler.py
+++ tests/test_handlers/test_template_handler.py
@@ -3,6 +3,7 @@ from clicksignlib.environments import SandboxEnvironment
 
 name = "any valid template name"
 access_token = "any valid token"
+api_version = "/api/v2"
 
 
 def test_ensure_TemplateHandler_can_be_imported_from_clicksignlib_handlers_package() -> None:
@@ -12,7 +13,7 @@ def test_ensure_TemplateHandler_can_be_imported_from_clicksignlib_handlers_packa
 def test_ensure_TemplateHandler_intantiation_params() -> None:
     env = clicksignlib.environments.SandboxEnvironment()
     sut = clicksignlib.handlers.TemplateHandler(
-        access_token=access_token, environment=env
+        access_token=access_token, environment=env, api_version=api_version
     )
     assert sut._access_token == access_token
     assert sut._environment == env
@@ -21,6 +22,6 @@ def test_ensure_TemplateHandler_intantiation_params() -> None:
 def test_TemplateHandler_base_endpoint_returns_the_env_endpoint_() -> None:
     env = SandboxEnvironment()
     sut = clicksignlib.handlers.TemplateHandler(
-        access_token=access_token, environment=env
+        access_token=access_token, environment=env, api_version=api_version
     )
     sut.base_endpoint == env.endpoint
