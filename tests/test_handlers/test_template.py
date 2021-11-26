c9958a47de919e9daff48a8a042a982c3f47d454
Erick Duarte <erickod@gmail.com>
2021-11-26 14:01:10 -0300

    refactor: name is not required in constructor anymore

--- tests/test_handlers/test_template.py
+++ tests/test_handlers/test_template.py
@@ -1,5 +1,6 @@
 from unittest.mock import Mock
 
+import pytest
 from clicksignlib.handlers import Template
 
 name = "any valid template name"
@@ -13,16 +14,16 @@ def test_handlers_has_a_template_package() -> None:
 
 
 def test_Template_instantiation_params() -> None:
-    sut = Template(name=name)
+    sut = Template()
 
-    assert sut._name == name
+    assert sut._path == ""
     assert sut._content == b""
     assert sut._status_code == 0
 
 
 def test_Template_from_bytes_is_saved_in_content_property() -> None:
-    sut = Template(name=name)
-    sut.from_bytes(data_bytes)
+    sut = Template()
+    sut.from_bytes("test.docx", data_bytes)
 
     assert sut._content == data_bytes
 
@@ -31,35 +32,30 @@ def test_Template_from_file_calls_from_bytes_with_right_params() -> None:
     target_file = "test.docx"
 
     with open("test.docx", "rb") as f:
-        sut = Template(name=name)
+        sut = Template()
         sut.from_bytes = Mock()
         sut.from_file(target_file)
         sut.from_bytes.assert_called_with(f.read())
 
 
 def test_Template_as_dict_method_return() -> None:
-    sut = Template(name=name)
+    sut = Template()
 
     assert sut.as_dict() == {
         "template[content]": sut._content,
-        "template[name]": sut._name,
+        "template[name]": sut.name,
     }
 
 
 def test_Template_is_valid_return_true_when_everything_goes_well() -> None:
-    sut = Template(name=name)
-    sut.from_bytes(data_bytes)
+    sut = Template()
+    sut.from_bytes("test.docx", data_bytes)
 
     assert sut.is_valid()
 
 
-def test_Template_is_valid_return_false_when_name_is_invalid() -> None:
-    sut = Template(name=name)
+def test_Template_is_valid_raises_when_name_or_content_is_invalid() -> None:
+    sut = Template()
 
-    assert not sut.is_valid()
-
-
-def test_Template_is_valid_return_false_when_content_is_invalid() -> None:
-    sut = Template(name=name)
-
-    assert not sut.is_valid()
+    with pytest.raises(ValueError):
+        sut.is_valid()
