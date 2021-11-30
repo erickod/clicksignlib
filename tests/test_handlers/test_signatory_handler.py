2cbddc66c0b217e0aeff7664014ffef1238f91ad
Erick Duarte <erickod@gmail.com>
2021-11-30 12:25:49 -0300

    test: ensure add_signatory_to_document method calls post from request

--- tests/test_handlers/test_signatory_handler.py
+++ tests/test_handlers/test_signatory_handler.py
@@ -2,6 +2,7 @@ from unittest.mock import Mock
 
 import clicksignlib
 from clicksignlib.environments import SandboxEnvironment
+from clicksignlib.handlers import SignerType
 
 env = SandboxEnvironment()
 access_token: str = "899ee68c-0a4a-48dc-9331-44844203b6b4"
@@ -32,5 +33,16 @@ def test_SignatoryHandler_create_method() -> None:
         email="erickduarte@companyhero.com",
         birthday="1990-10-15",
         phone_number="6198464580",
+    )
+    requests.post.assert_called_once()
+
+
+def test_SignatoryHandler_add_signatory_to_document_calls_requests() -> None:
+    requests = Mock()
+    sut = clicksignlib.handlers.SignatoryHandler(
+        access_token=access_token, environment=env, requests_adapter=requests
+    )
+    sut.add_signatory_to_document(
+        "document_key", "signer_key", SignerType.SURETY, "message"
     )
     requests.post.assert_called_once()
