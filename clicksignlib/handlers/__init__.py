80d4b788d00aa883030c70e716dcff8fe5a85b11
Erick Duarte <erickod@gmail.com>
2021-11-30 11:56:25 -0300

    feat: add NotificationHandler

--- clicksignlib/handlers/__init__.py
+++ clicksignlib/handlers/__init__.py
@@ -1,5 +1,6 @@
 from . import mixins
 from .base_handler import Config
 from .document_handler import DocumentHandler
-from .signatory_handler import SignatoryHandler
+from .notification_handler import NotificationHandler
+from .signatory_handler import SignatoryHandler, SignerType
 from .template_handler import TemplateHandler
--- clicksignlib/handlers/signatory_handler/__init__.py
+++ clicksignlib/handlers/signatory_handler/__init__.py
@@ -1,1 +1,2 @@
 from .signatory_handler import SignatoryHandler
+from .signer_type import SignerType
