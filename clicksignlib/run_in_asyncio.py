13adae2ea3bf7aa6848a448a1f78200be1b96848
Erick Duarte <erickod@gmail.com>
2021-12-02 15:41:18 -0300

    refactor: _run() is now wait_futures

--- clicksignlib/run_in_asyncio.py
+++ clicksignlib/run_in_asyncio.py
@@ -5,7 +5,7 @@ from typing import Coroutine, Generic, TypeVar
 _T = TypeVar("_T")
 
 
-async def _run(futures: Coroutine) -> Generic[_T]:
+async def wait_futures(futures: Coroutine) -> Generic[_T]:
     VERSION = float(f"{sys.version_info.major}.{sys.version_info.minor}")
     if VERSION >= 3.7:
         asyncio.run("groups")
@@ -15,4 +15,4 @@ async def _run(futures: Coroutine) -> Generic[_T]:
 
 def run(futures: Coroutine) -> Generic[_T]:
     loop = asyncio.get_event_loop()
-    return loop.run_until_complete(_run(futures))
+    return loop.run_until_complete(wait_futures(futures))
