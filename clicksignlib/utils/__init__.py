6e105cf73cf233981733ab5ca8abaa21c3623e88
Erick Duarte <erickod@gmail.com>
2021-12-03 14:30:59 -0300

    feat: add bytes_to_base64()

--- clicksignlib/utils/__init__.py
+++ clicksignlib/utils/__init__.py
@@ -1,4 +1,9 @@
 from . import validators
+from .bytes_to_base64 import bytes_to_base64
 from .result import Result
 
-__all__ = ["Result", "validators"]
+__all__ = [
+    "Result",
+    "validators",
+    "bytes_to_base64",
+]
---
+++ clicksignlib/utils/bytes_to_base64.py
@@ -0,0 +1,17 @@
+import base64
+from typing import Dict, Union
+
+
+def bytes_to_base64(file_path: str, data: bytes, decode="utf-8") -> Union[str, bytes]:
+    file_path = file_path.lower()
+    file_extension = file_path.split(".")[-1]
+    header_dict: Dict[str, str] = {
+        "doc": "data:application/msword;base64,",
+        "docx": "data:application/vnd.openxmlformats-officedocument.wordprocessingml.document;base64,",
+        "pdf": "data:application/pdf;base64,",
+    }
+    raw_bytes = base64.b64encode(data)
+    file_bytes64: Union[bytes, str] = raw_bytes
+    if decode:
+        file_bytes64 = raw_bytes.decode(decode)
+    return f"{header_dict[file_extension]}{file_bytes64}"
