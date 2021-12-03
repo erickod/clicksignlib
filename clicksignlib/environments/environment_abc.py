161c985e22dfc10cf34543ba3435cb23178ba073
Erick Duarte <erickod@gmail.com>
2021-12-03 17:31:30 -0300

    refactor: removed unused method

--- clicksignlib/environments/environment_abc.py
+++ clicksignlib/environments/environment_abc.py
@@ -5,11 +5,3 @@ class EnvironmentABC(ABC):
     def __init__(self, title: str, endpoint: str) -> None:
         self.title = title
         self.endpoint = endpoint
-
-    def is_valid(self):
-        required_class_variables = ["title", "endpoint"]
-        for var in required_class_variables:
-            if not getattr(self, var):
-                raise NotImplementedError(
-                    f"Provide a value to the `{var}` attributte in the {self.__class__.__name__} class"
-                )
--- tests/test_environments.py
+++ tests/test_environments.py
@@ -23,12 +23,6 @@ def test_ProductionEnvironment_exists_in_environments_package() -> None:
     assert hasattr(clicksignlib.environments, "ProductionEnvironment")
 
 
-def test_EnvironmentABC_raises_on_calling_is_valid() -> None:
-    with pytest.raises(NotImplementedError):
-        sut = EnvironmentABC(title="", endpoint="")
-        sut.is_valid()
-
-
 def test_ProductionEnvironment_default_instantiation_params() -> None:
     sut = ProductionEnvironment()
     assert sut.title == "Production Environment"
