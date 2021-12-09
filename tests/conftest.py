7599c5dbdfcac64bc30fd6b52819c65fb52fd72c
Erick Duarte <erickod@gmail.com>
2021-12-09 10:08:03 -0300

    feat: add patch fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,15 @@
 import pytest
-from unittest.mock import Mock, MagicMock
+from unittest.mock import Mock, MagicMock, patch as upatch
 
 
 @pytest.fixture
 def mock():
     return Mock()
 
-
 @pytest.fixture
 def magic_mock():
     return MagicMock()
+
+@pytest.fixture
+def patch(*args, **kwargs):
+    return upatch
