d3669a60f2998d1a3e7110f05600c9d6bb9fe1dd
Erick Duarte <erickod@gmail.com>
2021-12-09 15:39:58 -0300

    feat: add Auth

--- clicksignlib/handlers/__init__.py
+++ clicksignlib/handlers/__init__.py
@@ -3,7 +3,7 @@ from .config import Config
 from .document_handler import DocumentHandler
 from .embedded_handler import EmbeddedHandler
 from .notification_handler import NotificationHandler
-from .signatory_handler import SignatoryHandler, SignerType
+from .signatory_handler import Auth, SignatoryHandler, SignerType
 from .template_handler import TemplateHandler
 
 __all__ = [
@@ -13,6 +13,7 @@ __all__ = [
     "EmbeddedHandler",
     "NotificationHandler",
     "SignatoryHandler",
+    "Auth",
     "SignerType",
     "TemplateHandler",
 ]
