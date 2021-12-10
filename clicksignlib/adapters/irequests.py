29bfd7830f8b1e73d46e8bcd02868436e84a02cc
Erick Duarte <erickod@gmail.com>
2021-12-09 22:46:56 -0300

    feat: add files param on post method

--- clicksignlib/adapters/irequests.py
+++ clicksignlib/adapters/irequests.py
@@ -7,7 +7,7 @@ class IRequest(Protocol):
     async def get(self, url: str) -> Coroutine:
         pass
 
-    async def post(self, url: str, json) -> Coroutine:
+    async def post(self, url: str, json, files=None) -> Coroutine:
         pass
 
     async def put(self, url: str, json) -> Coroutine:
