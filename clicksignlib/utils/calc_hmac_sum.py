f78833e99a580aea8c8a44197d605dcf506e20c3
Erick Duarte <erickod@gmail.com>
2021-12-15 20:42:54 -0300

    refactor: add suppress to the AtributeError

--- clicksignlib/utils/calc_hmac_sum.py
+++ clicksignlib/utils/calc_hmac_sum.py
@@ -1,10 +1,17 @@
+import contextlib
 import hashlib
 import hmac
 
 
-def calc_hmac_sum(request_signature_key: str, secret_hmac_sha256: str) -> str:
+def calc_hmac_sum(data, secret_hmac_sha256) -> str:
+    with contextlib.suppress(AttributeError):
+        data = data.encode()
+
+    with contextlib.suppress(AttributeError):
+        secret_hmac_sha256 = secret_hmac_sha256.encode()
+
     return hmac.new(
-        secret_hmac_sha256.encode(),
-        msg=request_signature_key.encode(),
+        key=secret_hmac_sha256,
+        msg=data,
         digestmod=hashlib.sha256,
     ).hexdigest()
